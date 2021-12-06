4ab4c4e7460d3e07696c43b35a7763247a57d752
NicholasKiefer <nicholas.kiefer2@gmail.com>
2021-12-06 13:19:23 +0100

    day6, meh

--- day6/expo.py
+++ day6/expo.py
@@ -3,12 +3,21 @@ def expo_first(data):
 
 
 def expo_second(data):
-    return sum((project(i, 256) for i in data))
+    fishcount = [0] * 10
+    for i in range(8):
+        fishcount[i] = sum([j == i for j in data])
+    for _ in range(256):
+        fishcount[9] = fishcount[0]
+        for i in range(6):
+            fishcount[i] = fishcount[i + 1]
+        fishcount[6] = fishcount[7] + fishcount[9]
+        fishcount[7] = fishcount[8]
+        fishcount[8] = fishcount[9]
+    ret = sum(fishcount[:9])
+    return ret
 
 
 def project(init, days):
-    # at days - init we have two with counter 8 and 6
-    # afterwards its the same for every fish
     days_left = days - init
     if days_left <= 0:
         return 1
@@ -19,5 +28,5 @@ if __name__ == "__main__":
     with open("data.txt", "r") as file:
         d = file.read().split(",")
     d = [int(i) for i in d]
-    print(expo_first([4]))
-    # print(expo_second([1]))
+    print(expo_first(d))
+    print(expo_second(d))
