30582ec1906842aa639b3e6553510a60bebefbe1
NicholasKiefer <nicholas.kiefer2@gmail.com>
2021-12-15 12:23:20 +0100

    ich werd immer noch nicht A* aufschreiben

--- day15/simple_path.py
+++ day15/simple_path.py
@@ -2,9 +2,19 @@ from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
 import numpy as np
 
+
+def gm(ma):
+    ma = (ma + 1) % 10
+    ma = np.where(ma == 0, 1, ma)
+    return ma
+
+
 with open("data.txt", "r") as file:
     data = file.readlines()
-matrix = np.array([[int(i) for i in x[:-1]] for x in data])
+m = np.array([[int(i) for i in x[:-1]] for x in data])
+tile = np.concatenate([m, gm(m), gm(gm(m)), gm(gm(gm(m))), gm(gm(gm(gm(m))))], 1)
+tile = np.concatenate([tile, gm(tile), gm(gm(tile)), gm(gm(gm(tile))), gm(gm(gm(gm(tile))))], 0)
+matrix = m  # or for part 2 matrix = tile
 grid = Grid(matrix=matrix)
 start = grid.node(0, 0)
 end = grid.node(len(matrix[-1]) - 1, len(matrix) - 1)
